0b37a12d07c6abfc57c6c3c80010a63bfc5a1a8f
Matheus <matheusrcdtms@hotmail.com>
2024-01-05 14:24:04 -0300

    Remoção do securityweb

--- src/main/java/br/com/bradesco/bpw/callbackhandler/config/SecurityConfig.java
+++ src/main/java/br/com/bradesco/bpw/callbackhandler/config/SecurityConfig.java
@@ -3,14 +3,11 @@ package br.com.bradesco.bpw.callbackhandler.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
